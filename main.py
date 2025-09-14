27727e5b9b0b2b1b44909f078ff7cf4c5d99c241
Thomas LaPinta <tlapinta2002@gmail.com>
2025-09-14 15:18:54 -0400

    Implemented rendering of game rules

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ from Utility.userInput import UserInput
 from GameLib.gameSettings import GameSettings
 import GameLib.state as state   # import the module
 from GameLib.gameMessages import GameMessages
+from Utility.renderMessages import RenderMessages
 
 def main():
     # Initialize variables to store for game settings
-    player2Type = UserInput.getPlayer2Type()
-    finalScore = UserInput.getFinalScore()
+    RenderMessages.renderWelcomeMessage()
+    # player2Type = UserInput.getPlayer2Type()
+    # finalScore = UserInput.getFinalScore()
 
     # Update the gameSettings variable in the state module
-    state.gameSettings = GameSettings(finalScore, player2Type)
+    # state.gameSettings = GameSettings(finalScore, player2Type)
 
-    print(GameMessages.getTotalScores())
+    # print(GameMessages.getTotalScores())
 
 if __name__ == "__main__":
     main()
