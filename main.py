76d89f46666bb7abe8b4bf10eeeb4599d4b56ee0
Thomas LaPinta <tlapinta2002@gmail.com>
2025-09-13 20:43:43 -0400

    Added Ability for score updates

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-# Import Statements
+# main.py
 from Utility.userInput import UserInput
 from GameLib.gameSettings import GameSettings
-from GameLib.state import gameSettings
+import GameLib.state as state   # import the module
+from GameLib.gameMessages import GameMessages
 
 def main():
     # Initialize variables to store for game settings
     player2Type = UserInput.getPlayer2Type()
     finalScore = UserInput.getFinalScore()
 
-    gameSettings = GameSettings(finalScore, player2Type)
+    # Update the gameSettings variable in the state module
+    state.gameSettings = GameSettings(finalScore, player2Type)
+
+    print(GameMessages.getTotalScores())
 
 if __name__ == "__main__":
     main()
